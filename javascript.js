5fde4be833b1b3029d908bb17a1cb5695086edbe
shannoj <69207315+shannoj@users.noreply.github.com>
2023-06-15 18:04:32 +0200

    wrote the mouse over event listener to change the color of the divs

--- javascript.js
+++ javascript.js
@@ -1,23 +1,56 @@
+var gridCount = 0;
 
-function createGrid(rows, columns){
+function createGrid(){
+
     var container = document.createElement('div');
-    container.className = "container";
-    var rows = rows;
-    var columns = columns;
-    for (var i = 0; i < columns; ++i){
+    container.setAttribute('id', 'container');
+    var userRow = document.getElementById('userRow');
+    var userColumn = document.getElementById('userColumn');
+    for (var i = 0; i < userColumn.value; ++i){
     var column = document.createElement('div');
-    column.className = 'column';
-        for (var j = 0; j < rows; ++j){
+    column.setAttribute('id','column-' + (i+1));
+    column.classList.add('column');
+        for (var j = 0; j < userRow.value; ++j){
             var row = document.createElement('div');
-            row.className = "row";
+            row.setAttribute('id', 'square-column-' + (i+1) + '-row-' + (j+1));
+            row.classList.add('row');
             column.appendChild(row);
         }
         container.appendChild(column);
     }
     document.body.appendChild(container);
+    gridCount += 1;
+    console.log(gridCount)
 }
 
-var userRow = prompt("How many rows?");
-var userColumns = prompt("How many colums?");
+var enter = document.getElementById('enter');
 
-createGrid(userRow, userColumns);
+enter.addEventListener('click', function(){
+  createGrid();
+  var userRow = document.getElementById('userRow');
+  var userColumn = document.getElementById('userColumn');
+  var go = document.getElementById('enter');
+  userColumn.style.display = 'none';
+  userRow.style.display = 'none';
+  go.style.display = 'none';
+  
+  var row = document.getElementsByClassName('row');
+
+  for (var i = 0; i < row.length; i++) {
+    row[i].addEventListener('mouseover', function(e){
+        e.target.style.backgroundColor = 'blue';
+    });
+  }
+
+  var restart = document.createElement('button');
+  restart.classList.add('restart');
+  restart.textContent = 'restart';
+  document.body.appendChild(restart);
+  restart.addEventListener('click', function(){
+    for (var i = 0; i < row.length; i++) {
+        document.remove(row[i]);
+      }
+   });
+
+
+})
